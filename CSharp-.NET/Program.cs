f821fb5e356edfa19fb2a3814e3f2485f7941ccb
Guidotti <vinicius@cliqx.com.br>
2021-12-16 23:50:46 -0300

    initial commit

---
+++ CSharp-.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
